e7307f9379971fbf413dab23c735cb5472ff0e86
Robert Shepherd <rshepherdcpp@gmail.com>
2026-03-11 14:05:59 +0000

    fix the pacman movement so that it can only stay to the columns and rows.

--- pacman2/Pacman.hpp
+++ pacman2/Pacman.hpp
@@ -9,7 +9,7 @@
 
 struct Pacman {
 	Pacman(sf::Texture& pc_tx) : pm_sp(pc_tx) {
-		pm_sp.setPosition(sf::Vector2f(250, 250));
+		pm_sp.setPosition(sf::Vector2f(240, 240));
 	}
 
 	auto draw(sf::RenderWindow& rw) {
@@ -17,13 +17,53 @@ struct Pacman {
 	}
 
 	auto update() {
+		std::cout << pm_sp.getPosition().x << ", " << pm_sp.getPosition().y << "\n";
+		if (to_move) {
+			if ((int(pm_sp.getPosition().x) % 20 == 0) and (int(pm_sp.getPosition().y) % 20 == 0)) {
+				dir = directions[move_to];
+				to_move = false;
+			}
+		}
+
 		pm_sp.move(sf::Vector2f(float(dir.first), float(dir.second)));
 	}
 
-	auto move_up() { dir = directions["up"]; }
-	auto move_down() { dir = directions["down"]; }
-	auto move_left() { dir = directions["left"]; }
-	auto move_right() { dir = directions["right"]; }
+	auto move_up() {  
+		if (dir == directions["none"]) {
+			dir = directions["up"];
+		}
+		else {
+			to_move = true;
+			move_to = "up";
+		}
+	}
+	auto move_down() { 
+		if (dir == directions["none"]) {
+			dir = directions["down"];
+		}
+		else {
+			to_move = true;
+			move_to = "down";
+		}
+	}
+	auto move_left() { 
+		if (dir == directions["none"]) {
+			dir = directions["left"];
+		}
+		else {
+			to_move = true;
+			move_to = "left";
+		}
+	}
+	auto move_right() { 
+		if (dir == directions["none"]) {
+			dir = directions["right"];
+		}
+		else {
+			to_move = true;
+			move_to = "right";
+		}
+	}
 
 private:
 	sf::Sprite pm_sp;
@@ -37,4 +77,6 @@ private:
 	};
 	std::pair<int, int> dir = directions["none"];
 
+	bool to_move = false;
+	std::string move_to{};
 };
