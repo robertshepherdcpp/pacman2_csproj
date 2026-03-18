9574dd7e270296fd21288af15656f6b82ade6b49
Robert Shepherd <rshepherdcpp@gmail.com>
2026-03-18 17:56:26 +0000

    i have found the error - it is in the implementation of the path tracing algorithm (i.e. find_shortest_path)

--- pacman2/Blinky.hpp
+++ pacman2/Blinky.hpp
@@ -67,46 +67,56 @@ struct Blinky {
 		bl_sp.setPosition(sf::Vector2f(float(p.first * 20), float(p.second * 20)));
 	}
 	auto update(Pacman& p, Map& m) {
-		// the position of the pacman
-		auto crd = p.getCoord();
-		sf::Vector2f pos{ float(crd.first * 20), float(crd.second * 20) };
-
-		// first check that they arent intersecting, i.e. no collision
-		auto my_pos = bl_sp.getPosition();
-		if (abs(my_pos.x - pos.x) < 20 and abs(my_pos.y - pos.y) < 20) {
-			// then we have a collision.
-			if (!eadible) {
-				p.die();
-			}
-			else {
-				die();
-			}
+		// just testing code
+		auto pacman_pos = p.getCoord();
+		std::cout << "finding shortest path.\n";
+		std::vector<std::pair<int, int>> vec = shortest_path(m, position, pacman_pos);
+		// now we print the shortest path
+		for (int i = 0; i < vec.size(); i++) {
+			std::cout << i << ": " << vec[i].first << ", " << vec[i].second << "\n";
 		}
-		else {
-			// they arent intersecting, so we have to update the path that should be taken.
-			auto pacman_pos = p.getCoord();
-			if (pacman_pos == last_position) { // checking if there is any point in recalculating.
-				// same as last time, then we dont calculate a new path
-			}
-			else {
-				// calculate the shortest path
-				moves = shortest_path(m, position, pacman_pos);
-				index = 0;
-			}
-			// if there are any moves to be had:
-			if (moves.size() > 0) {
-				// move the ghost
-				auto move = moves[index];
-				bl_sp.move(sf::Vector2f(float(move.first), float(move.second)));
-				auto new_pos = bl_sp.getPosition();
-				index += 1;
+		
+		// the ret of the code (the original written code)
+		//// the position of the pacman
+		//auto crd = p.getCoord();
+		//sf::Vector2f pos{ float(crd.first * 20), float(crd.second * 20) };
 
-				// update the coordinate of the ghost, if it is at a coordinate that is directly on a grid cell.
-				if (int(new_pos.x) % 20 == 0 and int(new_pos.y) % 20 == 0) {
-					position = { float(new_pos.x / 20), float(new_pos.y / 20) };
-				}
-			}
-		}
+		//// first check that they arent intersecting, i.e. no collision
+		//auto my_pos = bl_sp.getPosition();
+		//if (abs(my_pos.x - pos.x) < 20 and abs(my_pos.y - pos.y) < 20) {
+		//	// then we have a collision.
+		//	if (!eadible) {
+		//		p.die();
+		//	}
+		//	else {
+		//		die();
+		//	}
+		//}
+		//else {
+		//	// they arent intersecting, so we have to update the path that should be taken.
+		//	auto pacman_pos = p.getCoord();
+		//	if (pacman_pos == last_position) { // checking if there is any point in recalculating.
+		//		// same as last time, then we dont calculate a new path
+		//	}
+		//	else {
+		//		// calculate the shortest path
+		//		moves = shortest_path(m, position, pacman_pos);
+		//		index = 0;
+		//	}
+		//	// if there are any moves to be had:
+		//	if (moves.size() > 0) {
+		//		// move the ghost
+		//		auto move = moves[index];
+		//		bl_sp.move(sf::Vector2f(float(move.first), float(move.second)));
+		//		auto new_pos = bl_sp.getPosition();
+		//		index += 1;
+
+		//		// update the coordinate of the ghost, if it is at a coordinate that is directly on a grid cell.
+		//		if (int(new_pos.x) % 20 == 0 and int(new_pos.y) % 20 == 0) {
+		//			position = { float(new_pos.x / 20), float(new_pos.y / 20) };
+		//		}
+		//	}
+		//}
 	}
 
 	auto die() -> void {
